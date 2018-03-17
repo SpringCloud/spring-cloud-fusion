1d3b4acf99bf89def2a1532744198704ddc50b7a
xujin <Software_King@qq.com>
2018-03-18 00:14:00 +0800

    init

---
+++ src/main/java/cn/springcloud/fusion/springcloudfusion/SpringCloudFusionApplication.java
@@ -0,0 +1,12 @@
+package cn.springcloud.fusion.springcloudfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFusionApplication.class, args);
+	}
+}
